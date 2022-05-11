9b43b9f7839ad1c7b9c9046d27978586fe79bb72
Yuriy Brovko <ysb.ptc@gmail.com>
2022-05-11 19:28:38 +0300

    01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+const bodyEl = document.querySelector('body');
+
+let timerColor = null;
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerColor = setInterval(() => {
+    bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerColor);
+});
