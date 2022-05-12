902222a85f6df8776ca7a52313b47a73d84f9065
Yuriy Brovko <ysb.ptc@gmail.com>
2022-05-12 13:31:50 +0300

    //

---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
